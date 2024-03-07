0f945ef27d8f6a02131f623866d9de585e57c963
aizazulllhaq <aizazulllhaq@gmail.com>
2024-03-07 11:30:43 +0500

    feat - Minor Syntax Problem Solve

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -5,16 +5,18 @@ const { wrapAsync } = require("../utils/wrapAsync");
 
 exports.getUserDetail = wrapAsync(async (req, res, next) => {
 
+    // Getting user detail from req.user
     const { _id, fullname, email, profileImage, levelAccount, coins } = req.user;
 
-    res.json({
+    res.status(200).json({
+        success: true,
         user: _id, fullname, email, profileImage, levelAccount, coins
     })
 })
 
 
 exports.startMining = async (req, res, next) => {
-    const userId = req.user._id; // Assuming you have the user ID stored in req.user
+    const userId = req.user._id; // Extract userID from req.user
 
     const currentTime = new Date();
 
@@ -32,9 +34,9 @@ exports.startMining = async (req, res, next) => {
         setTimeout(async () => {
             await User.findByIdAndUpdate(userId, { $set: { status: false } });
             console.log('User status reset after 24 hours.');
-        }, 24 * 60 * 60 * 1000); // 24 hours in milliseconds
+        }, 24 * 60 * 60 * 1000);
 
-        res.status(200).json({ message: 'Mining started. You can mine again after 24 hours.' });
+        res.status(200).json({ success: true, message: 'Mining started. You can mine again after 24 hours.', user: updatedUser });
     } catch (error) {
         console.error('Error updating user:', error);
         res.status(500).json({ message: 'Internal server error.' });
